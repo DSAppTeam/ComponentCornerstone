9ad6432542e1a78c5160ff53b905c169a7da8681
yummylau <yummyl.lau@gmail.com>
2019-07-29 17:41:13 +0800

    extension嵌套编译

--- module-plugin/src/main/groovy/com/plugin/module/ModulePlugin.groovy
+++ module-plugin/src/main/groovy/com/plugin/module/ModulePlugin.groovy
@@ -1,16 +1,38 @@
 package com.plugin.module
 
-import com.plugin.module.alone.AloneExtension
-import com.plugin.module.alone.AlonePlugin
+import com.plugin.module.extention.ModuleExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class ModulePlugin implements Plugin<Project> {
 
+    static File misDir
+
     @Override
     void apply(Project project) {
 
-        new AlonePlugin().apply(project)
+        if (project == project.rootProject) {
+            project.getExtensions().create("module", ModuleExtension, project.objects)
+            misDir = new File(project.projectDir, '.gradle/mis')
+            if (!misDir.exists()) {
+                misDir.mkdirs()
+            }
+
+            project.gradle.getStartParameter().taskNames.each {
+                if (it == 'clean') {
+                    if (!misDir.deleteDir()) {
+                        throw new RuntimeException("unable to delete dir " + misDir.absolutePath)
+                    }
+                    misDir.mkdirs()
+                }
+            }
+
+            project.repositories {
+                flatDir {
+                    dirs misDir.absolutePath
+                }
+            }
+        }
     }
 
 }
