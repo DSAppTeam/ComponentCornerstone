c71898424c79236e0fb2b055b15dabd103129722
yummylau <yummyl.lau@gmail.com>
2019-07-29 18:46:11 +0800

    update code

--- module-plugin/src/main/groovy/com/plugin/module/ModulePlugin.groovy
+++ module-plugin/src/main/groovy/com/plugin/module/ModulePlugin.groovy
@@ -1,35 +1,79 @@
 package com.plugin.module
 
-import com.plugin.module.extention.ModuleExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
+import com.plugin.module.alone.AloneExtension
+import com.plugin.module.mis.extension.MisExtension
+import com.plugin.module.mis.extension.OnMisExtensionListener
+import com.plugin.module.mis.extension.Publication
+import com.plugin.module.mis.extension.PublicationManager
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class ModulePlugin implements Plugin<Project> {
 
     static File misDir
+    static MisExtension mis
+    static AloneExtension alone
+    static PublicationManager publicationManager
 
     @Override
     void apply(Project project) {
 
-        if (project == project.rootProject) {
-            project.getExtensions().create("module", ModuleExtension, project.objects)
-            misDir = new File(project.projectDir, '.gradle/mis')
-            if (!misDir.exists()) {
+        boolean isRootProject = project == project.rootProject
+
+        if (isRootProject) {
+            initPlugin(project)
+        }
+
+    }
+
+    private void initPlugin(Project project) {
+        misDir = new File(project.projectDir, '.gradle/mis')
+        if (!misDir.exists()) {
+            misDir.mkdirs()
+        }
+        project.gradle.getStartParameter().taskNames.each {
+            if (it == 'clean') {
+                if (!misDir.deleteDir()) {
+                    throw new RuntimeException("unable to delete dir " + misDir.absolutePath)
+                }
                 misDir.mkdirs()
             }
-
-            project.gradle.getStartParameter().taskNames.each {
-                if (it == 'clean') {
-                    if (!misDir.deleteDir()) {
-                        throw new RuntimeException("unable to delete dir " + misDir.absolutePath)
-                    }
-                    misDir.mkdirs()
-                }
+        }
+        project.repositories {
+            flatDir {
+                dirs misDir.absolutePath
             }
+        }
+        publicationManager = PublicationManager.getInstance()
+        publicationManager.loadManifest(project, misDir)
 
-            project.repositories {
+        mis = project.getExtensions().create("mis", MisExtension, new OnMisExtensionListener() {
+            @Override
+            void onPublicationAdded(Project childProject, Publication publication) {
+                initPublication(childProject, publication)
+                publicationManager.addDependencyGraph(publication)
+            }
+        })
+        alone = project.getExtensions().create("runalone", AloneExtension)
+
+
+        project.allprojects.each {
+            if (it == project) return
+            Project childProject = it
+            childProject.repositories {
                 flatDir {
                     dirs misDir.absolutePath
+
+                }
+
+
+                childProject.plugins.whenObjectAdded {
+                    if (it instanceof AppPlugin || it instanceof LibraryPlugin) {
+//                    childProject.pluginManager.apply('mis')
+                        def del = childProject.plugins
+                    }
                 }
             }
         }
