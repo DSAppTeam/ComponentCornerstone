98352f23c167ce5dda4643f798991cc870eb5844
yummylau <yummyl.lau@gmail.com>
2019-07-31 15:19:14 +0800

    add mis sourceset

--- module-plugin/src/main/groovy/com/plugin/module/ModulePlugin.groovy
+++ module-plugin/src/main/groovy/com/plugin/module/ModulePlugin.groovy
@@ -1,14 +1,17 @@
 package com.plugin.module
 
+import com.android.build.gradle.BaseExtension
 import com.plugin.module.extension.AloneExtension
 import com.plugin.module.extension.module.AssembleTask
 import com.plugin.module.extension.module.Dependencies
 import com.plugin.module.extension.MisExtension
+import com.plugin.module.extension.module.SourceFile
+import com.plugin.module.extension.module.SourceSet
 import com.plugin.module.listener.OnMisExtensionListener
 import com.plugin.module.publication.Publication
 import com.plugin.module.publication.PublicationManager
+import com.plugin.module.transform.CodeTransform
 import com.plugin.module.utils.MisUtil
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.maven.MavenPublication
@@ -39,44 +42,89 @@ class ModulePlugin implements Plugin<Project> {
      */
     private void handleProject(Project project) {
 
-//        //获取运行task名称
-//        String taskNames = project.gradle.startParameter.taskNames.toString()
-//        Logger.buildOutput("taskNames is " + taskNames)
-//
-//        //获取运行模块名称
-//        String module = project.path.replace(":", "")
-//        Logger.buildOutput("current module is " + module)
-//
-//        //解析AssembleTask
-//        Logger.buildOutput("startParameter.taskNames is " + project.gradle.startParameter.taskNames)
-//        AssembleTask assembleTask = com.plugin.module.utils.Utils.parseTaskInfo(project.gradle.startParameter.taskNames)
-//        if (assembleTask.isAssemble) {
-//            compileModule = com.plugin.module.utils.Utils.parseMainModuleName(project, assembleTask)
-//            Logger.buildOutput("compile module is : " + compileModule)
-//        }
-//
-//        //需要在特定的模块中声明 isRunAlone，用于判断是否单独运行
-//        if (!project.hasProperty(Constants.PROPERTIES_ISRUNALONE)) {
-//            throw new RuntimeException("you should set isRunAlone in " + module + "'s gradle.properties")
-//        }
-//
-//        boolean isRunAlone = Boolean.parseBoolean((project.properties.get("isRunAlone")))
-//        String mainModuleName = project.rootProject.property(Constants.PROPERTIES_MAIN_MODULE_NAME)
-//
-//        //当且仅当 isRunAlone 为ture需要判断
-//        if (isRunAlone && assembleTask.isAssemble) {
-//            //如果运行的模块就是app模块，或者当前运行的模块就是我们配置的mainmodulename，则默认需要单独运行，其他组件强制修改为false
-//            if (module.equals(compileModule)) {
-//                isRunAlone = true
-//            } else {
-//                isRunAlone = false
-//            }
-//        }
-//        project.setProperty("isRunAlone", isRunAlone)
-//        Logger.buildOutput("setProperty isRunAlone(" + isRunAlone + ")")
+        String compileModule = Constants.DEFAULT_APP_NAME
+
+        project.getExtensions().create("runalone", AloneExtension)
+
+        def misScript = new File(project.projectDir, 'module.gradle')
+        if (misScript.exists()) {
+            project.apply from: misScript
+        } else {
+            throw new RuntimeException("找不到 " + project.name + "下的module.gradle ！")
+        }
+
+        //获取运行task名称
+        String taskNames = project.gradle.startParameter.taskNames.toString()
+        Logger.buildOutput("taskNames is " + taskNames)
+
+        //获取运行模块名称
+        String module = project.path.replace(":", "")
+        Logger.buildOutput("current module is " + module)
+
+        //解析AssembleTask
+        Logger.buildOutput("startParameter.taskNames is " + project.gradle.startParameter.taskNames)
+        AssembleTask assembleTask = com.plugin.module.utils.Utils.parseTaskInfo(project.gradle.startParameter.taskNames)
+        if (assembleTask.isAssemble) {
+            compileModule = com.plugin.module.utils.Utils.parseMainModuleName(project, assembleTask)
+            Logger.buildOutput("compile module is : " + compileModule)
+        }
+
+        //需要在特定的模块中声明 isRunAlone，用于判断是否单独运行
+        if (!project.hasProperty(Constants.PROPERTIES_ISRUNALONE)) {
+            throw new RuntimeException("you should set isRunAlone in " + module + "'s gradle.properties")
+        }
+
+//        boolean isRunAlone = extension.runAlone
 
 
-        new AlonePlugin().apply(project)
+        boolean isRunAlone = Boolean.parseBoolean((project.properties.get("isRunAlone")))
+        String mainModuleName = project.rootProject.property(Constants.PROPERTIES_MAIN_MODULE_NAME)
+
+
+        //当且仅当 isRunAlone 为ture需要判断
+        if (isRunAlone && assembleTask.isAssemble) {
+            //如果运行的模块就是app模块，或者当前运行的模块就是我们配置的mainmodulename，则默认需要单独运行，其他组件强制修改为false
+            if (module.equals(compileModule)) {
+                isRunAlone = true
+            } else {
+                isRunAlone = false
+            }
+        }
+        project.setProperty("isRunAlone", isRunAlone)
+        Logger.buildOutput("setProperty isRunAlone(" + isRunAlone + ")")
+        boolean needAloneSourceSrt = false;
+
+        //根据配置添加各种组件依赖，并且自动化生成组件加载代码
+        if (isRunAlone) {
+            project.apply plugin: Constants.PLUGIN_APPLICATION
+            Logger.buildOutput("project.apply plugin: com.android.application")
+
+            //对于组件，则需要读取alone目录进行运行
+            if (!module.equals(mainModuleName)) {
+                needAloneSourceSrt = true;
+
+                project.android.sourceSets {
+                    main {
+                        manifest.srcFile Constants.AFTER_MANIFEST_PATH
+                        java.srcDirs = [Constants.JAVA_PATH, Constants.AFTER_JAVA_PATH]
+                        res.srcDirs = [Constants.RES_PATH, Constants.AFTER_RES_PATH]
+                        assets.srcDirs = [Constants.ASSETS_PATH, Constants.AFTER_ASSETS_PATH]
+                        jniLibs.srcDirs = [Constants.JNILIBS_PATH, Constants.AFTER_JNILIBS_PATH]
+                    }
+                }
+
+            }
+            if (assembleTask.isAssemble && module.equals(compileModule)) {
+                com.plugin.module.utils.Utils.compileComponents(project, assembleTask)
+                //参考https://github.com/luojilab/DDComponentForAndroid/issues/122
+                project.extensions.findByType(BaseExtension.class).registerTransform(new CodeTransform(project));
+//                project.android.registerTransform(new CodeTransform(project))
+            }
+        } else {
+            project.apply plugin: Constants.PLUGIN_LIBRARY
+            Logger.buildOutput("project.apply plugin: com.android.library")
+        }
+
 
 //        if (!MisUtil.hasAndroidPlugin(project)) {
 //            throw new GradleException("The android or android-library plugin must be applied to the project.")
@@ -104,9 +152,14 @@ class ModulePlugin implements Plugin<Project> {
             }
         }
 
+
         project.afterEvaluate {
             //调整sourceSet
             MisUtil.addMisSourceSets(project)
+
+//            if(needAloneSourceSrt){
+//                MisUtil.addAloneSourceSet(project)
+//            }
 
 
             List<Publication> publicationList = publicationManager.getPublicationByProject(project)
@@ -182,6 +235,7 @@ class ModulePlugin implements Plugin<Project> {
 
         //评估完成之后
         project.afterEvaluate {
+
             androidJarPath = MisUtil.getAndroidJarPath(project, misExtension.compileSdkVersion)
 
             //扩展misPublication
@@ -449,6 +503,36 @@ class ModulePlugin implements Plugin<Project> {
             }
         }
 
+    }
+
+    void initPublication(Project project, Publication publication) {
+        String displayName = project.getDisplayName()
+        publication.project = displayName.substring(displayName.indexOf("'") + 1, displayName.lastIndexOf("'"))
+        def buildMis = new File(project.projectDir, 'build/mis')
+
+        publication.sourceSetName = publication.name
+        publication.buildDir = new File(buildMis, publication.name)
+
+        SourceSet misSourceSet = new SourceSet()
+        def misDir
+        if (publication.sourceSetName.contains('/')) {
+            misDir = new File(project.projectDir, publication.sourceSetName + '/mis/')
+        } else {
+            misDir = new File(project.projectDir, 'src/' + publication.sourceSetName + '/mis/')
+        }
+        misSourceSet.path = misDir.absolutePath
+        misSourceSet.lastModifiedSourceFile = new HashMap<>()
+        project.fileTree(misDir).each {
+            if (it.name.endsWith('.java') || it.name.endsWith('.kt')) {
+                SourceFile sourceFile = new SourceFile()
+                sourceFile.path = it.path
+                sourceFile.lastModified = it.lastModified()
+                misSourceSet.lastModifiedSourceFile.put(sourceFile.path, sourceFile)
+            }
+        }
+
+        publication.misSourceSet = misSourceSet
+        publication.invalid = misSourceSet.lastModifiedSourceFile.isEmpty()
     }
 
 }
