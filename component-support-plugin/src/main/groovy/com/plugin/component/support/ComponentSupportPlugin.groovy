af382ddec432fa7e65aeeac772153cfe08243790
yummylau <yummyl.lau@gmail.com>
2019-08-29 22:20:01 +0800

    分离support插件功能

--- component-support-plugin/src/main/groovy/com/plugin/component/support/ComponentSupportPlugin.groovy
+++ component-support-plugin/src/main/groovy/com/plugin/component/support/ComponentSupportPlugin.groovy
@@ -1,6 +1,8 @@
 package com.plugin.component.support
 
+import com.android.build.gradle.BaseExtension
 import com.plugin.component.support.extension.ComponentSupportExtension
+import com.plugin.component.support.transform.MethodCostTransform
 import com.plugin.component.support.utils.ProjectUtil
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -19,10 +21,8 @@ class ComponentSupportPlugin implements Plugin<Project> {
         if (!isRoot) {
             return
         }
-
         extension = project.getExtensions().create(Constants.COMPONENT_SUPPORT, ComponentSupportExtension.class)
         project.afterEvaluate {
-
 
             Set<String> includeModules = ProjectUtil.getModuleName(extension.includes)
             Set<String> excludeModules = ProjectUtil.getModuleName(extension.excludes)
@@ -33,17 +33,22 @@ class ComponentSupportPlugin implements Plugin<Project> {
                 Project childProject = it
                 String projectName = ProjectUtil.getModuleName(childProject)
                 if (includeModel && includeModules.contains(projectName)) {
-                    childProject.apply plugin: 'com.android.component.support'
-                    childProject.dependencies {
-                        implementation 'com.effective.android:component-support-core:1.0.0'
-                    }
+                    addPluginToProject(childProject)
                 } else if (!excludeModules.contains(projectName)) {
-                    childProject.apply plugin: 'com.android.component.support'
-                    childProject.dependencies {
-                        implementation 'com.effective.android:component-support-core:1.0.0'
-                    }
+                    addPluginToProject(childProject)
                 }
             }
+        }
+    }
+
+
+    void addPluginToProject(Project project) {
+        project.apply plugin: 'com.android.component.support'
+        project.dependencies {
+            implementation 'com.effective.android:component-support-core:1.0.0'
+        }
+        if (extension.methodCostEnable) {
+            project.extensions.findByType(BaseExtension.class).registerTransform(new MethodCostTransform(project))
         }
     }
 }
