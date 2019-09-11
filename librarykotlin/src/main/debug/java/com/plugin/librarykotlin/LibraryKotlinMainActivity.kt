50a5e552cd7faa5fb328ac9ad221aa8e3db22aae
yummylau <yummyl.lau@gmail.com>
2019-09-11 15:15:23 +0800

    test 循环编译

--- librarykotlin/src/main/debug/java/com/plugin/librarykotlin/LibraryKotlinMainActivity.kt
+++ librarykotlin/src/main/debug/java/com/plugin/librarykotlin/LibraryKotlinMainActivity.kt
@@ -3,6 +3,9 @@ package com.plugin.librarykotlin
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.plugin.component.ComponentManager
+import com.plugin.component.SdkManager
+import com.plugin.library.ISdk
 
 import com.plugin.librarykotlin.R
 
@@ -11,6 +14,7 @@ class LibraryKotlinMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
-        Toast.makeText(this, KotlinSdkImpl().getKotlinSdkName(), Toast.LENGTH_LONG).show()
+        ComponentManager.init(application)
+        Toast.makeText(this, SdkManager.getSdk(ISdk::class.java)!!.sdkName, Toast.LENGTH_LONG).show()
     }
 }
